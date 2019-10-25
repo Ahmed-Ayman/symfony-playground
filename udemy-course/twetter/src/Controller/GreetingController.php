61ab39adde25939b88bc582890432dd6a4247e64
ahmed-ayman <ahmedayman055g@gmail.com>
2019-10-25 23:10:37 +0200

    create a greeting service.

---
+++ udemy-course/twetter/src/Controller/GreetingController.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\GreentingService\Greeting;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GreetingController extends AbstractController
+{
+
+    /**
+     * @var Greeting
+     */
+    private $greeting;
+
+    public function __construct(Greeting $greeting)
+    {
+        $this->greeting = $greeting;
+    }
+
+    /**
+     * @Route("/", name="home_index")
+     *
+     * @param Request $request
+     */
+    public function greetName(Request $request)
+    {
+      return  $this->render('base.html.twig', [
+            'greeting_message' => $this->greeting->greet($request->get('name') ?? 'no name')
+        ]);
+    }
+}
---
+++ udemy-course/twetter/src/GreentingService/Greeting.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\GreentingService;
+
+
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class Greeting
+ * @package App\GreentingService
+ *
+ * This service is:
+ * - returning and logging hi to a name string passed to i
+ *
+ * TODO: test this service.
+ *
+ * give it ahmed -> logs and returns hi to ahmed.
+ */
+class Greeting
+{
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+
+        $this->logger = $logger;
+    }
+
+    public function greet($name ): string
+    {
+        $this->logger->info("Hi $name ");
+        return "Hi $name";
+    }
+}

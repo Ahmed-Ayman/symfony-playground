cadb0775ba11252cf413789e1ef4f246c9e8b3b0
ahmed-ayman <ahmedayman055g@gmail.com>
2019-10-31 15:20:35 +0200

    create login form.

--- udemy-course/twetter/src/DataFixtures/AppFixtures.php
+++ udemy-course/twetter/src/DataFixtures/AppFixtures.php
@@ -3,13 +3,32 @@
 namespace App\DataFixtures;
 
 use App\Entity\MicroPost;
+use App\Entity\User;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var UserPasswordEncoder
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
+    {
+        $this->loadMicroPosts($manager);
+        $this->loadUsers($manager);
+    }
+
+    private function loadMicroPosts(ObjectManager $manager)
     {
         for ($i = 0; $i < 10; $i++) {
             $microPost = new MicroPost();
@@ -19,4 +38,16 @@ class AppFixtures extends Fixture
         }
         $manager->flush();
     }
+
+    private function loadUsers(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('ahmed' );
+        $user->setPassword($this->encoder->encodePassword($user, 'password'));
+        $user->setFullname('Ahmed Ayman');
+        $user->setEmail('ahmedayman055g@gmail.com');
+        $manager->persist($user);
+        $manager->flush();
+    }
+
 }

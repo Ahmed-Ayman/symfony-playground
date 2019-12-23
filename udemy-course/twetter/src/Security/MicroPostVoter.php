03e5b343c251a21c0e938d2ac89023ff003f2133
ahmed-ayman <ahmedayman055g@gmail.com>
2019-12-23 17:04:00 +0200

    add micropost edit/delete voter.

--- udemy-course/twetter/src/Security/MicroPostVoter.php
+++ udemy-course/twetter/src/Security/MicroPostVoter.php
@@ -6,17 +6,25 @@ namespace App\Security;
 
 use App\Entity\MicroPost;
 use App\Entity\User;
+use mysql_xdevapi\Exception;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\Exception\LogicException;
+use Symfony\Component\Security\Core\User\UserInterface;
 
+/***
+ * Class MicroPostVoter
+ * @package App\Security
+ */
 class MicroPostVoter extends Voter
 {
-
     const EDIT = 'edit';
-    const DELTE = 'delete';
+    const DELETE = 'delete';
+
 
     /**
-     * Determines if the attribute/action and subject are supported by this voter.
+     * Determines if the attribute and subject are supported by this voter.
      *
      * @param string $attribute An attribute
      * @param mixed $subject The subject to secure, e.g. an object the user wants to access or any other PHP type
@@ -25,7 +33,7 @@ class MicroPostVoter extends Voter
      */
     protected function supports($attribute, $subject)
     {
-        if (!in_array($attribute, [self::DELTE, self::EDIT])) {
+        if (!in_array($attribute, [self::EDIT, self::DELETE])) {
             return false;
         }
         if (!$subject instanceof MicroPost) {
@@ -39,19 +47,40 @@ class MicroPostVoter extends Voter
      * It is safe to assume that $attribute and $subject already passed the "supports()" method check.
      *
      * @param string $attribute
-     * @param mixed $subject
+     * @param MicroPost $microPost
      *
      * @return bool
      */
-    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    protected function voteOnAttribute($attribute, $microPost, TokenInterface $token)
     {
-        // checking the actual permissions.
-        $authenticatedUser = $token->getUser();
-        if (!$authenticatedUser instanceof User) {
-            return false;
+        $user = $token->getUser();
+        switch ($attribute) {
+            case self::DELETE:
+                return $this->canDelete($microPost, $user);
+            case self::EDIT:
+                return $this->canEdit($microPost, $user);
         }
 
-        /* @var MicroPost $subject */
-        return $subject->getUser()->getId() === $authenticatedUser->getId();
+        throw  new LogicException("Access fucking to edit/delete is denied.");
+    }
+
+    private function canDelete(MicroPost $microPost, User $user)
+    {
+        if ($microPost->getUser()->getId() === $user->getId()) {
+            return true;
+        } else {
+            throw  new AccessDeniedException("Fuck you can't delete");
+        }
+
+    }
+
+    private function canEdit(MicroPost $microPost, User $user)
+    {
+
+        if ($microPost->getUser()->getId() === $user->getId()) {
+            return true;
+        } else {
+            throw  new AccessDeniedException("Fuck you can't edit");
+        }
     }
 }
